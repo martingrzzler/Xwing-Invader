a218b5e70412cd25f2463c87ab057cd09ff53c72
Martin Gressler <martin.gressler@code.berlin>
2019-10-09 16:13:23 +0200

    making a lot of changes. the ship moves now to left and right and stop on either side!

--- ship.py
+++ ship.py
@@ -6,7 +6,9 @@ class Ship:
     def __init__(self, ai_game):
         '''Initialize the ship and set it's starting position.'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+        
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/Xwing.bmp')
@@ -14,6 +16,23 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Moving flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''Update the ship's position based on the movement flag'''
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x        
 
     def blitme(self):
         '''Draw the ship at its current location.'''
